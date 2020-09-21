346e37eece41c9820d067ecf8ff11d85d06325f8
storgatonicola <storgatonicola@provincia.treviso.it>
2020-09-21 17:40:48 +0200

    add dropdown-search (livewire)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movie App</title>
     <link rel="stylesheet" href="/css/main.css">
+    @livewireStyles
 </head>
 <body class="font-sans bg-gray-900 text-white">
 
@@ -29,19 +30,12 @@
             </ul>
 
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text" class="bg-gray-800 rounded-full text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="w-4 fill-current text-gray-500 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
-                        </svg>
-                    </div>
-                </div>
+                <livewire:search-dropdown>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
                         <img src="/img/avatar.jpg" alt="avatar" class="rounded-full h-8 w-8">
                     </a>
-                </div>
+                </div>           
             </div>
 
 
@@ -49,6 +43,6 @@
     </nav>
 
     @yield('content')
-    
+    @livewireScripts
 </body>
 </html>
